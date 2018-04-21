471c82ce8b9c04442e1962bdebdb0c6e2339ad39
Alex <rus@gmail.com>
2018-04-21 22:30:06 +0300

    password file check in run and test scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if ! [ -f ethereum/keys/ethereum/password ]
+then
+  echo "Password file is not found, please find README.txt and make sure you have both keystore and password in place"
+  exit -1
+fi
+
 docker-compose up -d --scale worker=2 
--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if ! [ -f ethereum/keys/test/password ]
+then
+  echo "Password file is not found, please find README.txt and make sure you have both keystore and password in place"
+  exit -1
+fi
+
 docker-compose -f test_docker-compose.yml up
